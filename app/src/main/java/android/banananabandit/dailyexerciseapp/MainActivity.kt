5c25d164a8f96a44976066ad20d0171178803c88
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-09-14 09:12:24 +0100

    Creating custom Start button + main layout

--- app/src/main/java/android/banananabandit/dailyexerciseapp/MainActivity.kt
+++ app/src/main/java/android/banananabandit/dailyexerciseapp/MainActivity.kt
@@ -2,10 +2,18 @@ package android.banananabandit.dailyexerciseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frameLayoutStartButton : FrameLayout = findViewById(R.id.frame_layout_start)
+
+        frameLayoutStartButton.setOnClickListener {
+            Toast.makeText(this, "press", Toast.LENGTH_SHORT).show()
+        }
     }
 }
