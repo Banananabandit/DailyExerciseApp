5e7bde09fada00a4ce59d49ec0c0d1e307a9eb21
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-09-15 07:17:15 +0100

    Adding ViewBinding

--- app/src/main/java/android/banananabandit/dailyexerciseapp/MainActivity.kt
+++ app/src/main/java/android/banananabandit/dailyexerciseapp/MainActivity.kt
@@ -1,19 +1,29 @@
 package android.banananabandit.dailyexerciseapp
 
+import android.banananabandit.dailyexerciseapp.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val frameLayoutStartButton : FrameLayout = findViewById(R.id.frame_layout_start)
-
-        frameLayoutStartButton.setOnClickListener {
+        binding.frameLayoutStart.setOnClickListener {
             Toast.makeText(this, "press", Toast.LENGTH_SHORT).show()
         }
+
+        // how much code is reduced through viewBinding
+//        val frameLayoutStartButton : FrameLayout = findViewById(R.id.frame_layout_start)
+//        frameLayoutStartButton.setOnClickListener {
+//            Toast.makeText(this, "press", Toast.LENGTH_SHORT).show()
+//        }
     }
+    
 }
