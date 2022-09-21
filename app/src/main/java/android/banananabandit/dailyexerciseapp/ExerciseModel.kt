c798125e4d38e9c5690e5fd20e057b9113a96f09
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-09-21 12:59:09 +0100

    Fixing the model (noob mistake)

--- app/src/main/java/android/banananabandit/dailyexerciseapp/ExerciseModel.kt
+++ app/src/main/java/android/banananabandit/dailyexerciseapp/ExerciseModel.kt
@@ -5,4 +5,32 @@ class ExerciseModel(
     private var name : String,
     private var isCompleted : Boolean,
     private var isSelected : Boolean
-)
+) {
+    fun getId() : Int {
+        return id
+    }
+
+    fun setId(id : Int) {
+        this.id = id
+    }
+
+    fun getName() : String {
+        return name
+    }
+    fun setName(name : String) {
+        this.name = name
+    }
+    fun getIsExerciseCompleted() : Boolean {
+        return isCompleted
+    }
+    fun setExerciseCompleted(isCompleted: Boolean) {
+        this.isCompleted = isCompleted
+    }
+
+    fun getIsExerciseSelected() : Boolean {
+        return isSelected
+    }
+    fun setExerciseSelected(isSelected : Boolean) {
+        this.isSelected = isSelected
+    }
+}
